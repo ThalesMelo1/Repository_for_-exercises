976f778ebdef38f508b147256256e856e6427fdc
ThalesMelo1 <thales.melo@sga.pucminas.br>
2025-05-12 12:45:48 +0000

    Atividade soma finalizada (#3)

--- C/Verde/Soma Serie.c
+++ C/Verde/Soma Serie.c
@@ -3,11 +3,31 @@
 int main()
 {
     int num;
-    float soma = 0;
+    float soma = 0, cont = 1;
     scanf("%d", &num);
-    for(int i = 0; i < num; i++)
+    if (num == 0)
     {
-        soma = (2+i)/(i+1);
-
+        soma = 0;
     }
+    else if (num < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        for (float i = 1; i <= num; i++)
+        {
+            if (i == 1)
+            {
+                soma += i / i;
+            }
+            else
+            {
+                soma += (cont + i) / i;
+                cont++;
+            }
+        }
+    }
+    printf("%f", soma);
+    return 0;
 }
---
+++ Python/Teste.py
@@ -0,0 +1,4 @@
+def add_numbers(a, b=2):
+    print(a + b + d)
+ 
+add_numbers(a=1, c=3)
