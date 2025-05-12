97022109ed83f0f9f3d4973bbc67fe40d44cb4ae
ThalesMelo1 <thales.melo@sga.pucminas.br>
2025-05-12 18:06:21 +0000

    Atividade Média Verde (#3)

---
+++ C/Verde/Média.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int NumPessoas, AcimaMedia = 0;
+    scanf("%d", &NumPessoas);
+    int Notas[NumPessoas];
+    float SomaNotas = 0, media;
+        for (int i = 0; i < NumPessoas; i++)
+        {
+            scanf("%d", &Notas[i]);
+            SomaNotas += Notas[i];
+        }
+        media = SomaNotas / NumPessoas;
+        for (int i = 0; i < NumPessoas; i++)
+        {
+            if (Notas[i] > media)
+            {
+                AcimaMedia++;
+            }
+        }
+        printf("%.3f%%", (float)AcimaMedia/NumPessoas*100);
+        return 0;
+}
