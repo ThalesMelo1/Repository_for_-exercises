a457c879b3487ef9eb900f7dfc90f6be1a9fe3c2
ThalesMelo1 <thales.melo@sga.pucminas.br>
2025-05-16 21:27:08 +0000

    p4 completo (#5)

--- C/Lista 12/p4.c
+++ C/Lista 12/p4.c
@@ -12,5 +12,13 @@ int main()
     real = (float*)malloc(sizeof(float));
     caractere = (char*)malloc(sizeof(char));
 
-    
+    printf("Digite um inteiro\n");
+    scanf("%d", &*inteiro);
+    printf("Digite um real\n");
+    scanf("%f", &*real);
+    printf("Digite um caractere\n");
+    scanf(" %c", &*caractere);
+    printf("Inteiro: %d\nReal: %.2f\nCaractere: %c\n", *inteiro, *real, *caractere);
+    printf("O inteiro esta alocado no espaco de memoria: %d\nO real esta alocado no espaco de memoria: %d\nO caractere esta alocado no espaco de memoria: %d\n", inteiro, real, caractere);
+    return 0;
 }
