e7655668790f8fd11287fce403e380c6ef4fde78
ThalesMelo1 <thales.melo@sga.pucminas.br>
2025-06-10 01:51:45 +0000

    asd

--- C/L14/p7.c
+++ C/L14/p7.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct
 {
@@ -9,8 +10,8 @@ typedef struct
 int main()
 {
     Ponto2d Ponto_inicial, Ponto_final;
-    Ponto_inicial.x, Ponto_inicial.y = -9999, -9999;
-    Ponto_final.x, Ponto_final.y = -9999, -9999;
+    Ponto_inicial.x = -9999; Ponto_inicial.y = -9999;
+    Ponto_final.x = -9999; Ponto_final.y = -9999;  
     float Distancia;
     int option; 
     do
@@ -19,25 +20,31 @@ int main()
         scanf("%d", &option);
         if(option == 1)
         {
+            printf("Escreva o x de seu numero ");
             scanf("%f", &Ponto_inicial.x);
+            printf("Agora escreva o y de seu numero ");
             scanf("%f", &Ponto_inicial.y);
         }
         if(option == 2)
         {
+            printf("Escreva o x de seu numero ");
             scanf("%f", &Ponto_final.x);
+            printf("Agora escreva o y de seu numero ");
             scanf("%f", &Ponto_final.y);
         }
         if(option == 3)
         {
-            if((Ponto_inicial.x = 9999)||(Ponto_inicial.y = 9999))
+            if((Ponto_inicial.x == -9999)||(Ponto_inicial.y == -9999)||(Ponto_final.x == -9999)||(Ponto_final.y == -9999))
             {
-
+                printf("Ponto não inserido. Tente novamente");
             }
-
-
+            else
+            {
+            Distancia = sqrt(pow(Ponto_final.x - Ponto_inicial.x, 2)+ pow(Ponto_final.y - Ponto_inicial.y, 2));
+            printf("A distancia eh: %.2f\n", Distancia);
+            }
         }
-
     } while(option != 4);
+    printf("FIM");
     return 0;
-
 }
