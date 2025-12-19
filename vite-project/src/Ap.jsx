9fb5d84d247bcde4cb47c6259906c476482da3a2
ThalesMelo1 <thales.melo@sga.pucminas.br>
2025-12-19 15:51:14 +0000

    tets

--- vite-project/src/Ap.jsx
+++ vite-project/src/Ap.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "/workspaces/Personal-Repository/vite-project/src/Pages/Home.jsx"
+import SignUp from "/workspaces/Personal-Repository/vite-project/src/Pages/SignUp.jsx"
 
 const Ap = () => {
 
     return (   
-        
+        <Routes>
+            <Route path = "/" element = {<Home />}/>
+            <Route path = "/signup" element = {<SignUp />}/>
+        </Routes>
         
        )
     }
---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const Home = () => {
+    return (
+    <div>
+        <p>
+            Hello World!
+        </p>
+    </div>
+    )
+}
